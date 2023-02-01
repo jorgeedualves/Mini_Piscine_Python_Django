e70898b6bc23a16747cc5d8f8a50eae22e1aee47
Jorge Eduardo alves <joeduard@c2r2p4.42sp.org.br>
2023-02-01 12:17:48 -0300

    ex00 day01 Ok

--- day00/ex00/myawesomescript.sh
+++ day00/ex00/myawesomescript.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 
 curl -s $1 | grep "http:" | cut -d '"' -f2
---
+++ day01/ex00/var.py
@@ -0,0 +1,24 @@
+def my_var():
+    int0 = 42
+    str0 = '42'
+    str1 = 'quarante-deux'
+    float0 = 42.
+    bool0 = True
+    list0 = [42]
+    dict0 = {42 :42}
+    tuple0 = (42,)
+    set0 = set()
+
+    print(f'{int0} has a type {type(int0)}')
+    print(str0," has a type ",type(str0))
+    print(f'{str1} has a type {type(str1)}')
+    print(f'{float0} has a type {type(float0)}')
+    print(f'{bool0} has a type {type(bool0)}')
+    print(f'{list0} has a type {type(list0)}')
+    print(f'{dict0} has a type {type(dict0)}')
+    print(f'{tuple0} has a type {type(tuple0)}')
+    print(f'{set0} has a type {type(set0)}')
+
+my_var()
+
+

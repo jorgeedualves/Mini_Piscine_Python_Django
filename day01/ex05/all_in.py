ed46a8edd37bc7415a0613294fbd4e0f3fab3b01
jorgeedualves <jorgeedualves84@gmail.com>
2023-02-07 12:10:36 -0300

    ex05 Ok

--- day01/ex05/all_in.py
+++ day01/ex05/all_in.py
@@ -1,4 +1,18 @@
 import sys
+def valid_states(input_user, states, capital_cities):
+        if (states[input_user] in capital_cities):
+            print(f'{capital_cities[states[input_user]]} is the capital of {input_user}')
+
+def valid_capital(input_user, states, capital_cities):
+    for key, value in capital_cities.items():
+        if (input_user == value):
+            new_value = key
+    for key, value in states.items():
+        if (new_value == value):
+            print(f'{input_user} is the capital of {key}')
+
+    
+
 def countries(input_user):
     states = {
     "Oregon" : "OR",
@@ -13,14 +27,17 @@ def countries(input_user):
     "CO": "Denver"
     }
 
-    if (input_user in capital_cities.values()):
-        print(f'{input_user}')
-    elif (input_user in states):
-        print(f'{input_user}')
-        if (states.values() == capital_cities.keys()):
-            print(f'{capital_cities.values()}')
-    else:
-        print(f'{input_user} is neither a capital city nor a state')
+    text = input_user.split(',')
+
+    for i in text:
+        inp = i.strip(' ').title()
+        if (inp != ''):
+            if (inp in states):
+                valid_states(inp, states, capital_cities)
+            elif (inp in capital_cities.values()):
+                valid_capital(inp, states, capital_cities)
+            else:
+                print(f'{inp} is neither a city nor a state')
 
 if __name__ == '__main__':
     if (len(sys.argv) != 2):
